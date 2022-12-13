58f0fddee09438dbfca215c5f742c2856264e609
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-12-13 18:30:19 -0500

    completed query Total Cohort Assistance Duration

---
+++ 4_queries/6_average_assistance_request_wait_time.sql
@@ -0,0 +1,1 @@
+SELECT avg(started_at - created_at) FROM assistance_requests;
---
+++ 4_queries/7_total_cohort_assistance_duration.sql
@@ -0,0 +1,8 @@
+SELECT
+    cohorts.name AS cohort,
+    sum(completed_at - started_at) AS total_duration
+FROM assistance_requests
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY total_durations
