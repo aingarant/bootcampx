bc1b9bb936db07635b14882af57e7055f0aad83b
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-12-13 19:38:23 -0500

    added most confusing assignment

--- 4_queries/7_total_cohort_assistance_duration.sql
+++ 4_queries/7_total_cohort_assistance_duration.sql
@@ -5,4 +5,4 @@ FROM assistance_requests
     JOIN students ON student_id = students.id
     JOIN cohorts ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
-ORDER BY total_durations
+ORDER BY total_duration
---
+++ 4_queries/8_cohort_average_assistance_duration.sql
@@ -0,0 +1,14 @@
+SELECT
+    avg(total_duration) AS average_total_duration
+FROM (
+        SELECT
+            cohorts.name AS cohort,
+            sum(completed_at - started_at) AS total_duration
+        FROM
+            assistance_requests
+            JOIN students ON student_id = students.id
+            JOIN cohorts ON cohorts.id = students.cohort_id
+        GROUP BY cohorts.name
+        ORDER BY
+            total_duration
+    ) AS total_duration
